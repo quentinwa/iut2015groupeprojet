bef74312b0a8dfac29e49ef238148a313060665c
QUENTIN WADOUX <wadoux.quentin@info-141-00.iutcb.univ-littoral.fr>
2015-10-09 13:38:04 +0200

    2eme commit

---
+++ app/js/app.js
@@ -0,0 +1,32 @@
+'use + +++++ + +6+++++ ++ + +strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
+
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/modifier', {
+        templateUrl: 'partials/modifier.html',
+        controller: 'ModifierCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
