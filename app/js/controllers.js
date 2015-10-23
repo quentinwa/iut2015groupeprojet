acb45fb2c33b8cbec977ea4ec4fd5529a0246b00
QUENTIN WADOUX <wadoux.quentin@info-141-00.iutcb.univ-littoral.fr>
2015-10-23 13:40:39 +0200

    Yolo

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,8 +10,8 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
     $scope.orderProp = 'age';
   }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone', '$http',
+  function($scope, $routeParams, Phone, $http) {
     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
       $scope.mainImageUrl = phone.images[0];
     });
@@ -19,4 +19,24 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Ph
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
     };
+    
+    $scope.sendEmail = function(){
+        
+        var req = {
+            method: 'POST',
+            url: 'https://api.mailgun.net/v3/sandbox1a772981c6814abd835afcc7c72dc269.mailgun.org/messages?to=quentinwa@laposte.net&from=quentinwa@laposte.net&text=salut',
+            headers: {
+                'Authorization': 'Basic YXBpOmtleS0xZTNlMzk0YzlkZmJkOTNkNmM1ZDlkNTNiYWViYTRiMw=='
+            }
+        };
+        
+        $http(req).success(function(response){
+            alert ('Message envoyé');
+        }).error(function(response){
+            alert ('Message non envoyé');
+        });
+    };
+    
   }]);
+  
+
